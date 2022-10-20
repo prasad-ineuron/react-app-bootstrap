f50fb37a1d4699bfa8f89f772e0ebf266299916a
prasad-ineuron <prasad@ineuron.ai>
2022-10-20 17:47:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to React + Bootstrap</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ineuron.ai"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Image
+            src="/logo-light.png"
+            height={70}
+            width={140}
+            alt="ineuron-logo"
+          />
         </a>
       </header>
     </div>
